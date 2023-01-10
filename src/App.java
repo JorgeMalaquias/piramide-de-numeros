32798e6793422b5561e273ddb7c92f16070e2787
jorge <jorgemalaquia@gmail.com>
2023-01-10 02:23:24 -0300

    done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner inputVar = new Scanner(System.in);
+        int number = inputVar.nextInt();
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
     }
 }
